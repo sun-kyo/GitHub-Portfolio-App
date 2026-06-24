23d93d3fa3c91c6e0c0307830c72474d15580631
sun-kyo <psk910903@gmail.com>
2026-06-24 23:42:21 +0900

    feat: implement github search form

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,26 @@
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, Text, TextInput, Pressable } from 'react-native';
 
 export default function HomeScreen() {
+  const [input, setInput] = useState('');
+
   return (
     <View>
-      <Text>GitHub Portfolio App</Text>
+      <Text>GitHub Portfolio</Text>
+      <TextInput
+        placeholder="username 입력"
+        value={input}
+        onChangeText={(e) => {
+          setInput(e);
+        }}
+      />
+      <Pressable
+        onPress={() => {
+          console.log(input);
+        }}
+      >
+        <Text>검색</Text>
+      </Pressable>
     </View>
   );
 }
